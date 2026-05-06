4646795b671e0368da198afc08c32aed0369b520
RamC Venkatasamy <ramc.venkatasamy@gmail.com>
2026-05-06 17:21:25 -0500

    Update Apps Script deployment URL

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.BLNO_CONFIG = {
   GOOGLE_CLIENT_ID: "776947524108-mb972fjd6gs9b813rt0tl80lhk8qej3l.apps.googleusercontent.com",
-  BACKEND_URL: "https://script.google.com/macros/s/AKfycbzPTccVrzCDgCC7pINEfDOLVI12uGikkaLah6im4sA93PXA8qY71HCBCVos4IeeKVOZ/exec",
+  BACKEND_URL: "https://script.google.com/macros/s/AKfycbzX7oLJjW1VBbVuta324IUvL7EjjUhccgeCvwyx3GGX4X0UP4zjTi92GK46JaMRFDzM/exec",
   DEFAULT_MONTH: "May-2026",
   ACADEMY_NAME: "BLno Badminton Academy"
 };
